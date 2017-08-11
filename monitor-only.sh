b9265d904f3c29feb1146d5788ffb19f65142aa2
logan lembke <logan@blackhillsinfosec.com>
2017-08-11 16:24:50 -0600

    Fix monitor-only for debian like systems

--- monitor-only.sh
+++ monitor-only.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-IFACE="eth1"
 
-for mode in rx tx sg tso gso gro ; do ethtool -K $IFACE $mode off; done
+MONITORS="eth0"
 
+for monitor in $MONITORS; do
+  if [ $IFACE = $monitor ]; then
+    for mode in rx tx sg tso gso gro ; do ethtool -K $IFACE $mode off; done
+    exit 0
+  fi
+done
